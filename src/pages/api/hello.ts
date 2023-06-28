a133590fc318d85e84b707f1bd15f5c89237f572
Mehran <realmehranasadi@gmail.com>
2023-06-28 12:43:00 +0330

    add mongodb and mongoose and make connection

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDb from "../../../utils/connectDb";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  connectDb();
 }
---
+++ utils/connectDb.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("please add the database url in .env file");
+}
+
+const DATABASE_URL: string = process.env.DATABASE_URL;
+
+let globalWithMongoose = global as typeof globalThis & {
+  mongoose: any;
+};
+
+let cached = globalWithMongoose.mongoose;
+
+if (!cached) {
+  cached = globalWithMongoose.mongoose = { conn: null, promise: null };
+}
+
+async function connectDb() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+    cached.promise = mongoose
+      .connect(DATABASE_URL, options)
+      .then((mongoose) => {
+        console.log("Connection has been established");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.log(error as Error);
+      });
+  }
+  cached.conn = await cached.promise;
+  console.log(cached.conn);
+  return cached.conn;
+}
+
+export default connectDb;
