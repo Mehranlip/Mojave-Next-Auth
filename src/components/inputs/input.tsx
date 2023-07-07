e57366f1ef117a7d149cb0af9cc91a9a1acb9b35
Mehran <realmehranasadi@gmail.com>
2023-07-07 22:18:34 +0330

    end input commponents

--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -10,7 +10,18 @@ interface IInputProps {
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
   const { name, label, type, icon, placeholder } = props;
-  return <></>;
+  return (
+    <div className="relative mt-1 ">
+      <div className="pointer-events-none absolute left-0 inset-y-0 flex items-center pl-1">
+        <span className="text-lg text-gray-600 ">{icon}</span>
+      </div>
+      <input
+        className="input-com w-full py-2 pr-7 pl-8 block outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-blue-500 focus:ring-2  "
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 
 export default Input;
