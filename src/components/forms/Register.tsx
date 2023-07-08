a5999386209533b85cc51984e589175532b772fd
Mehran <realmehranasadi@gmail.com>
2023-07-08 14:30:25 +0330

    add valid schema to input

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,12 +1,35 @@
 import * as React from "react";
 import Input from "../inputs/input";
 import { CiUser } from "react-icons/ci";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface IRegisterFromProps {}
+const FormSchema = z.object({
+  first_name: z
+    .string()
+    .min(2, "First name must be atleast 2 characters.")
+    .max(32, "First name must be lass than 32 characters.")
+    .regex(new RegExp("^[a-zA-z]+$^"), "No special characters allowed."),
+});
 
+type FormSchemaType = z.infer<typeof FormSchema>;
 const RegisterForm: React.FunctionComponent<IRegisterFromProps> = (props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <form className="my-8 text-sm">
+    <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
       <div className="gap-2 md:flex">
         <Input
           name="first_name"
@@ -14,7 +37,11 @@ const RegisterForm: React.FunctionComponent<IRegisterFromProps> = (props) => {
           type="text"
           icon={<CiUser />}
           placeholder="example"
+          register={register}
+          error={errors?.first_name?.message}
+          disabled={isSubmitting}
         />
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -1,25 +1,53 @@
 import * as React from "react";
 
+import { IoAlertCircle } from "react-icons/io5";
+
 interface IInputProps {
   name: string;
   label: string;
   type: string;
   icon: JSX.Element;
   placeholder: string;
+  register: any;
+  error: any;
+  disabled: boolean;
 }
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
-  const { name, label, type, icon, placeholder } = props;
+  const { name, label, type, icon, placeholder, register, error, disabled } =
+    props;
   return (
-    <div className="relative mt-1 ">
-      <div className="pointer-events-none absolute left-0 inset-y-0 flex items-center pl-1">
-        <span className="text-lg text-gray-600 ">{icon}</span>
+    <div className=" w-[100%]">
+      <label htmlFor={name} className="text-white">
+        {label}
+      </label>
+      <div className="relative mt-1 ">
+        <div
+          className="pointer-events-none absolute left-0 top-0.5 inset-y-0 flex items-center pl-1"
+          style={{ transform: `${error ? "translateY(-10px)" : ""}` }}
+        >
+          <span className="text-lg text-gray-600 ">{icon}</span>
+        </div>
+        <input
+          className="input-com w-full py-2 pr-7 pl-8 block outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-blue-500 focus:ring-2  "
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+          style={{
+            borderColor: `${error ? "#ED4337" : ""}`,
+          }}
+        />
+        {error && (
+          <div className="fill-red-500 absolute right-2 top-2.5 text-xl">
+            <IoAlertCircle fill="#ED4337" />
+          </div>
+        )}
+        {error && (
+          <p className="text-sm text-[#ed4337] mt-1 w-fit rounded-xl px-1">
+            {error}
+          </p>
+        )}
       </div>
-      <input
-        className="input-com w-full py-2 pr-7 pl-8 block outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-blue-500 focus:ring-2  "
-        type={type}
-        placeholder={placeholder}
-      />
     </div>
   );
 };
