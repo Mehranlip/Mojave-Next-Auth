1a89a0fddfaae03835d32dd7e1ab578b19c135de
Mehran <realmehranasadi@gmail.com>
2023-07-14 20:16:24 +0330

    send mail func

---
+++ utils/sendMail.ts
@@ -0,0 +1,73 @@
+import nodemailer from "nodemailer";
+import * as handlebars from "handlebars";
+
+export default async function sendMail(
+  to: string,
+  name: string,
+  image: string,
+  url: string,
+  subject: string,
+  template: string
+) {
+  const {
+    MAILING_EMAIL,
+    MAILING_PASSWORD,
+    SMTP_HOST,
+    SMTP_EMAIL,
+    SMTP_PASSWORD,
+    SMTP_PORT,
+  } = process.env;
+
+  let transporter = await nodemailer.createTransport({
+    // service: "gmail",
+    // auth: {
+    //   user: MAILING_EMAIL,
+    //   pass: MAILING_PASSWORD,
+    // },
+
+    port: Number(SMTP_PORT),
+    host: SMTP_HOST,
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  });
+  //-----Html replacment
+  const data = handlebars.compile(template);
+  const replacments = {
+    name: name,
+    email_link: url,
+    image: image,
+  };
+  const html = data(replacments);
+  //------------verify connection config
+  await new Promise((resolve, reject) => {
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("server is listening...");
+        resolve(success);
+      }
+    });
+  });
+  //---------send email
+  const options = {
+    from: MAILING_EMAIL,
+    to,
+    subject,
+    html,
+  };
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(options, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+}
